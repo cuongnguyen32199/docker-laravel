1492e8ff1231a1c9b5a25b65d71caba319499287
Nguyễn Mạnh Cường <nguyenmanhcuong03@vccorp.vn>
2021-03-18 17:39:08 +0700

    Add migration

---
+++ app/database/migrations/2021_03_18_100942_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->uuid('id')->unique();
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->foreign('owner_id')->references('id')->on('users');
+            $table->enum('status', ['new', 'doing', 'completed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
