5440d9680b927633f77dde6e7ba364fa1e8f7a50
Cuong Nguyen Manh <cuong.nguyen@vcc.exchange>
2021-05-17 14:13:09 +0700

    Remove auth scaffold

--- web/routes/web.php
+++ web/routes/web.php
@@ -3,21 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*
-|--------------------------------------------------------------------------
-| Assign middleware to all routes within a group
-|--------------------------------------------------------------------------
-*/
-
 Route::view('/{uri?}', 'welcome')->where('uri', '(.*)');
